762300717f8823aa67a58e78bea0c919886df419
k3csk3 <szilagyi.viktor89@gmail.com>
2022-08-29 19:36:56 +0200

    lefut

--- 08-other/exercises/03/solution/js/objectConverter.js
+++ 08-other/exercises/03/solution/js/objectConverter.js
@@ -1,1 +1,51 @@
+/*3. Készíts egy `objectConverter` nevű objektumot, ami az alábbi metódusokkal rendelkezik:
+   - `arrayToMap(array)`
+   - `arrayToSet(array)`
+   - `setToMap(set)`
+   - `setToArray(set)`
+   - `mapToArray(map)`
+   - `mapToSet(map)`
+
+A fenti metódusok értelemszerűen nem másra szolgálnak, mint az egyik összetett adattípusból konvertálják át az adatokat egy másikba. 
+Mindegyik metódus visszatérési értéke egy új Array, Map, vagy Set object.
+*/
+
+const objectConverter = {
+   arrayToMap(array) {
+      const arrToMap = new Map([]);
+      array.forEach((value, index) => {
+         arrToMap.set(index, value)
+      }
+      )
+      return arrToMap
+   },
+   arrayToSet(array) {
+      return new Set(array)
+   },
+   setToMap(set) {
+      const setToMap = new Map([]);
+      Array.from(set).forEach((value, index) => {
+         setToMap.set(index, value)
+      }
+      )
+      return setToMap
+   },
+   setToArray(set) {
+      return [...set]
+      //return Array.from(set)
+   },
+   mapToArray(map) {
+      const newArray = [];
+      map.forEach((value) => {
+         newArray.push(value)
+      });
+      return newArray
+   },
+   mapToSet(map) {
+      const mapToSet = [];
+      map.forEach((value) => mapToSet.push(value));
+      return new Set(mapToSet);
+   },
+}
+
 export default objectConverter;
